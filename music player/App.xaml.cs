30c80a57b460a4bd58c43cf5ed4643784bf7f0c1
Sahil bhatiya <64317865+sahilbhatiya@users.noreply.github.com>
2020-05-29 11:41:56 +0530

    Add files via upload

---
+++ music player/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace music_player
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
